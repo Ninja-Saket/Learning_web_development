9bc2cafec4a8c85b797cc87b4f41437700896811
Ninja <phaser.84059@gmail.com>
2021-10-22 03:14:37 +0530

    Setting up the cookie-parser

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,8 @@
 module.exports.home = function (req, res) {
+  console.log(req.cookies);
+  res.cookie("user_id", Math.random());
+  res.cookie("name", "Saket");
+  res.cookie("branch", "CSE");
   return res.render("home", {
     title: "Home",
   });
--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express");
 const PORT = 8000;
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
 const db = require("./config/mongoose");
 app.use(express.static("./assets"));
 app.use(expressLayouts);
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
+app.use(express.urlencoded());
+app.use(cookieParser());
 //For any route go to "./routes/index"
 app.use("/", require("./routes"));
 
 //setting up views
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
 app.listen(PORT, function (err) {
   if (err) {
     console.log("Error in running the server : ", err);
